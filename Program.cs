2d5b8ba61b440d3acf3f7cf7d2674923cccc18a0
Joey Biotti <joey@MacBook-Pro-2.local>
2017-10-14 16:31:57 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ltr_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter, letter1, letter2;
+
+            Console.Write("Input Letter ");
+            letter = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Input letter again ");
+            letter1 = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Input letter one more time ");
+            letter2 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2}", letter2, letter1, letter);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Daily C# Exercise
+
+Today's C# exercise. Its a console app that asks for three letters and then prints out those three letters in reverse order. 
